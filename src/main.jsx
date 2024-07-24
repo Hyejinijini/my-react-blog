5abf807151e01ef5620e794a607e7f69d59d3483
Hyejinijini <qkdgpwls001@gmail.com>
2024-07-24 14:01:25 +0900

    Update: main.jsx 파일 경로를 절대 경로로 변경
    main.jsx 파일 경로를 상대 경로에서 절대 경로로 변경했음.

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 
-import router from './router.jsx'
+import router from '@/router.jsx'
 
-import './index.css'
+import '@assets/styles/css/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
