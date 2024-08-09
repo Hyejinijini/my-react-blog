bf027394a01c448b05f9bf1f79726ec9996ff225
hojae-lee <chosloo2835@gmail.com>
2024-08-09 11:36:45 +0900

    review : About 컴포넌트화 + 커스텀훅 생성

---
+++ src/pages/about/hooks/useFilteredListHook.js
@@ -0,0 +1,23 @@
+import { useState, useEffect, useCallback } from 'react'
+
+const useFilteredListHook = (searchTerm, list) => {
+  const [filteredList, setFilteredList] = useState([]) // 필터링된 목록 상태 추가
+
+  useEffect(() => {
+    // 검색어에 따라 필터링된 목록 생성
+    if (list) {
+      const fList = list.filter(
+        (item) =>
+          item.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+          item.content.toLowerCase().includes(searchTerm.toLowerCase()) ||
+          item.label.toLowerCase().includes(searchTerm.toLowerCase()) ||
+          item.skill.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+      setFilteredList(fList)
+    }
+  }, [searchTerm, list])
+
+  return filteredList
+}
+
+export default useFilteredListHook
