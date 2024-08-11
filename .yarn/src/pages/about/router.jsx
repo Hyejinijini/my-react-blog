1db1d346d86f555030ef2f5a7ee48edd5f30e4d1
Hyejinijini <qkdgpwls001@gmail.com>
2024-08-11 11:48:47 +0900

    Update: SideBar 수정
    SideBar 를 레이아웃으로 옮기고 각 컴포넌트 경로 수정 (Home, About)

--- .yarn/src/pages/about/router.jsx
+++ .yarn/src/pages/about/router.jsx
@@ -1,19 +1,9 @@
 import About from '@pages/about/About.jsx'
-import AboutDetail from '@pages/about/components/AboutDetail.jsx'
-import DetailPage from '@pages/about/components/DetailPage.jsx'
 
 const router = [
   {
     path: 'about',
     element: <About />
-  },
-  {
-    path: 'about/:title',
-    element: <AboutDetail />
-  },
-  {
-    path: 'about/:id/detail/:id',
-    element: <DetailPage />
   }
 ]
 
--- .yarn/src/pages/home/Home.jsx
+++ .yarn/src/pages/home/Home.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 
 import MetaTags from '@common/components/MetaTags.jsx'
-import SideBar from '@common/components/SideBar'
 import ReadMe from '@pages/home/components/ReadMe.jsx'
 
 const Home = () => {
@@ -10,13 +9,8 @@ const Home = () => {
       <MetaTags subTitle={' | Home'} description={'블로그 메인 페이지입니다.'} keywords={'Home'} />
 
       <div className="max-w-screen-xl w-full mx-auto">
-        <div className="flex flex-col xl:ml-10 xl:mr-4 mx-auto lg:mx-auto lg:ml-8 md:ml-6 sm:flex-col md:flex-row max-w-screen-xl">
-          {/* 왼쪽 사이드바 */}
-          <SideBar />
-
-          {/* 메인 콘텐츠 */}
-          <ReadMe />
-        </div>
+        {/* 메인 콘텐츠 */}
+        <ReadMe />
       </div>
     </div>
   )
