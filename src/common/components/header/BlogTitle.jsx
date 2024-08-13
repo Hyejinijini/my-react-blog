b53232cc44b6351b2b4b80077a2225ced4baf169
Hyejinijini <qkdgpwls001@gmail.com>
2024-08-13 17:37:47 +0900

    update: BlogTitle 컴포넌트 수정
    className 을 prop 으로 받아오게 수정했음.

--- src/common/components/header/BlogTitle.jsx
+++ src/common/components/header/BlogTitle.jsx
@@ -1,11 +1,8 @@
-const BlogTitle = () => {
+const BlogTitle = ({ className }) => {
   return (
     <>
       {/* 블로그 제목 */}
-      <a
-        href="/"
-        className="text-md font-bold hover:bg-rose-100 hover:rounded-md py-1.5 -my-2 text-start w-full lg:w-auto flex items-center"
-      >
+      <a href="/" className={className}>
         HYEHYE
       </a>
     </>
