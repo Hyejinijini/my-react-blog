4f680e5d9cc8b9c3e10237b60cbecddc17bafdfa
Hyejinijini <qkdgpwls001@gmail.com>
2024-08-13 17:40:02 +0900

    add: Logo 컴포넌트 생성
    Header 컴포넌트 코드 정리를 위해서 로고 이미지 부분을 컴포넌트화 했음.

---
+++ src/common/components/header/Logo.jsx
@@ -0,0 +1,9 @@
+const Logo = ({ className }) => {
+  return (
+    <div className="ml-1">
+      <img src="/HYEHYE.svg" alt="로고 이미지" className={className} />
+    </div>
+  )
+}
+
+export default Logo
