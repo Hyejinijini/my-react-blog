3c82dc13476f35a87b9dfc670395608bd87382d9
Hyejinijini <qkdgpwls001@gmail.com>
2024-08-13 17:43:17 +0900

    update: Header 컴포넌트 수정
    불필요한 부분은 삭제하고 분리시킬 부분은 분리시킴.

--- src/common/components/header/Header.jsx
+++ src/common/components/header/Header.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 
-import DropdownButton from '@common/components/header/DropdownButton.jsx'
-import CalendarModal from '@common/components/header/CalendarModal.jsx'
 import BlogTitle from '@common/components/header/BlogTitle.jsx'
+import Logo from '@common/components/header/Logo.jsx'
 import HeaderList from '@common/components/header/HeaderList.jsx'
 
 const Header = () => {
@@ -10,20 +9,9 @@ const Header = () => {
     <header className="text-gray-600 bg-rose-50 border-b border-b-rose-200">
       <div className="flex p-4 pb-2 gap-3 items-center">
         <div className="flex gap-2 w-full items-center">
-          <div className="ml-1">
-            <img src="/HYEHYE.svg" alt="로고 이미지" className="w-12 h-12" />
-          </div>
-          <BlogTitle />
-        </div>
+          <Logo className="w-12 h-12" />
 
-        <div className="flex gap-2">
-          <DropdownButton />
-          <CalendarModal />
-          <img
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPnofQS0P6zgg_gbUeMaCPPKS7vD1PVGhr0Q&s"
-            alt="프로필 이미지"
-            className="max-w-none box-border border border-rose-100 w-8 h-8 rounded-full hover:cursor-pointer"
-          />
+          <BlogTitle className="text-md font-bold hover:bg-rose-100 hover:rounded-md py-1.5 -my-2 text-start w-full lg:w-auto flex items-center" />
         </div>
       </div>
 
