fa57a502c30d7d86b0221bb7f4a9b945097a6450
Hyejinijini <qkdgpwls001@gmail.com>
2024-08-14 18:02:17 +0900

    update: Logo 컴포넌트 주석 추가
    코드에 대한 설명 추가

--- src/common/components/header/Logo.jsx
+++ src/common/components/header/Logo.jsx
@@ -1,6 +1,7 @@
 const Logo = ({ className }) => {
   return (
     <div className="ml-1">
+      {/* 로고 이미지 부분 */}
       <img src="/HYEHYE.svg" alt="로고 이미지" className={className} />
     </div>
   )
