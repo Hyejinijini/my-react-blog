5b26d90b4adc6215a7dd8a5aabd54a1ffbae355c
Hyejinijini <qkdgpwls001@gmail.com>
2024-08-14 18:09:46 +0900

    add: ProfileDataFetcher 컴포넌트 추가
    SideBar 컴포넌트 내에서 데이터를 불러오는 코드를 컴포넌트화 함.

---
+++ src/common/components/sidebar/ProfileDataFetcher.jsx
@@ -0,0 +1,44 @@
+import { useState, useEffect } from 'react'
+import { useRequestHook } from '@common/hooks/useDataFetchHooks.js'
+
+// data URL
+import { PROFILE_URL } from '@api/keys/home/url.js'
+
+// common
+import Loading from '@common/components/etc/Loading.jsx'
+
+const ProfileDataFetcher = ({ setProfile }) => {
+  // 데이터 URL
+  const url = PROFILE_URL
+
+  // 커스텀 훅을 사용하여 API로부터 데이터 가져오기
+  const { state: apiProfile } = useRequestHook(url)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const loadProfile = () => {
+      // 로컬스토리지에서 데이터 가져오기
+      const storedProfile = localStorage.getItem('profile')
+      // 로컬스토리지에 데이터가 있을 경우에는 로컬스토리지에서 데이터를 가져오고
+      if (storedProfile) {
+        setProfile(JSON.parse(storedProfile))
+      } else {
+        // 로컬스토리지에 데이터가 없을 경우에는 API 에서 데이터를 가져온다.
+        if (apiProfile) {
+          setProfile(apiProfile)
+          localStorage.setItem('profile', JSON.stringify(apiProfile))
+        }
+      }
+      setLoading(false)
+    }
+
+    loadProfile()
+  }, [apiProfile, setProfile])
+
+  if (loading) return <Loading />
+
+  // 해당 컴포넌트는 아무런 UI 를 렌더링할 필요가 없으므로 null 을 return 하도록 했음.
+  return null
+}
+
+export default ProfileDataFetcher
