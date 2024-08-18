47f49bada3a1e0a7830b0c45b70a55a9c4a0851f
Hyejinijini <qkdgpwls001@gmail.com>
2024-08-18 10:28:47 +0900

    update: HomeMain.jsx 수정
    불필요한 컴포넌트 삭제 및 컴포넌트명 변경으로 인한 import 수정

--- src/pages/home/components/HomeMain.jsx
+++ src/pages/home/components/HomeMain.jsx
@@ -1,9 +1,10 @@
-import SelfIntro from '@pages/home/components/SelfIntro.jsx'
+import MyInfo from '@pages/home/components/MyInfo.jsx'
 
 const HomeMain = () => {
   return (
     <main className="flex-1 sm:mx-0 sm:mt-6 sm:p-4 md:p-1 md:mr-4 md:ml-0 mx-0 p-4 mt-6 lg:p-2">
-      <SelfIntro />
+      {/* 소개 부분 */}
+      <MyInfo />
     </main>
   )
 }
