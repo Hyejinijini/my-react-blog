aec06388c6a0c81dfadf480b028c09709ea54fa6
Hyejinijini <qkdgpwls001@gmail.com>
2024-08-18 10:27:06 +0900

    update: SelfIntro -> MyInfo 변경
    컴포넌트명 변경 및 코드 수정

---
+++ src/pages/home/components/MyInfo.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import { motion } from 'framer-motion'
+
+// css
+import styles from '@assets/styles/css/home/selfInfo.module.css'
+
+// components
+import InfoFront from '@pages/home/components/InfoFront.jsx'
+import InfoBack from '@pages/home/components/InfoBack.jsx'
+import TechStack from '@pages/home/components/TechStack.jsx'
+
+const MyInfo = () => {
+  const [isFlipped, setIsFlipped] = useState(false)
+
+  const handleCardClick = () => {
+    setIsFlipped((prev) => !prev)
+  }
+
+  console.log('Rendering SelfIntro: isFlipped =', isFlipped) // 디버깅 로그
+
+  return (
+    <div className="relative w-full h-96 shadow-xl shadow-rose-100">
+      {/* 애니메이션이 적용된 div */}
+      <motion.div
+        className={`${styles.card} ${isFlipped ? styles.cardFlipped : ''}`}
+        style={{ transformStyle: 'preserve-3d' }}
+        transition={{ duration: 0.6 }}
+      >
+        {/* 앞면 */}
+        <InfoFront isFlipped={isFlipped} handleCardClick={handleCardClick} />
+
+        {/* 뒷면 */}
+        <InfoBack isFlipped={isFlipped} handleCardClick={handleCardClick} />
+      </motion.div>
+
+      {/* 기술 스택 부분 */}
+      {!isFlipped && <TechStack />}
+    </div>
+  )
+}
+
+export default MyInfo
