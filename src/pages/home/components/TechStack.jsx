b3e3e41feaa4380a319a36c4f95fcbae684b848d
Hyejinijini <qkdgpwls001@gmail.com>
2024-08-20 13:56:53 +0900

    update: TechStack.jsx 수정
    새로운 아이콘들 추가

--- src/pages/home/components/TechStack.jsx
+++ src/pages/home/components/TechStack.jsx
@@ -6,16 +6,16 @@ import { SiTailwindcss } from 'react-icons/si'
 
 const TechStack = () => {
   return (
-    <div className="flex justify-center space-x-8 py-8">
+    <div className="flex flex-wrap justify-center space-x-8 py-8 max-w-4xl mx-auto gap-1">
       <motion.div
         className="group relative flex items-center justify-center"
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ delay: 0.2, type: 'spring', stiffness: 100 }}
       >
-        <FaReact className="h-16 w-16 text-blue-500 hover:scale-110 duration-200" />
-        <span className="absolute bottom-[-40px] left-1/2 transform -translate-x-1/2 items-center justify-center hidden group-hover:flex">
-          <span className="relative z-10 p-2 px-4 text-sm leading-none text-white bg-rose-600 shadow-lg rounded-md">
+        <FaReact className="h-16 w-16 text-blue-500 hover:scale-110 duration-200 z-0" />
+        <span className="absolute bottom-[-40px] left-1/2 transform -translate-x-1/2 items-center justify-center hidden group-hover:flex z-20">
+          <span className="relative p-2 px-4 text-sm leading-none text-white bg-rose-600 shadow-lg rounded-md">
             React
           </span>
         </span>
@@ -26,9 +26,9 @@ const TechStack = () => {
         animate={{ opacity: 1, y: 0 }}
         transition={{ delay: 0.4, type: 'spring', stiffness: 100 }}
       >
-        <FaHtml5 className="h-16 w-16 text-orange-500 hover:scale-110 duration-200" />
-        <span className="absolute bottom-[-40px] left-1/2 transform -translate-x-1/2 items-center justify-center hidden group-hover:flex">
-          <span className="relative z-10 p-2 px-4 text-sm leading-none text-white bg-rose-600 shadow-lg rounded-md">
+        <FaHtml5 className="h-16 w-16 text-orange-500 hover:scale-110 duration-200 z-0" />
+        <span className="absolute bottom-[-40px] left-1/2 transform -translate-x-1/2 items-center justify-center hidden group-hover:flex z-20">
+          <span className="relative p-2 px-4 text-sm leading-none text-white bg-rose-600 shadow-lg rounded-md">
             HTML5
           </span>
         </span>
@@ -39,9 +39,9 @@ const TechStack = () => {
         animate={{ opacity: 1, y: 0 }}
         transition={{ delay: 0.6, type: 'spring', stiffness: 100 }}
       >
-        <FaCss3Alt className="h-16 w-16 text-blue-700 hover:scale-110 duration-200" />
-        <span className="absolute bottom-[-40px] left-1/2 transform -translate-x-1/2 items-center justify-center hidden group-hover:flex">
-          <span className="relative z-10 p-2 px-4 text-sm leading-none text-white bg-rose-600 shadow-lg rounded-md">
+        <FaCss3Alt className="h-16 w-16 text-blue-700 hover:scale-110 duration-200 z-0" />
+        <span className="absolute bottom-[-40px] left-1/2 transform -translate-x-1/2 items-center justify-center hidden group-hover:flex z-20">
+          <span className="relative p-2 px-4 text-sm leading-none text-white bg-rose-600 shadow-lg rounded-md">
             CSS3
           </span>
         </span>
@@ -52,9 +52,9 @@ const TechStack = () => {
         animate={{ opacity: 1, y: 0 }}
         transition={{ delay: 0.8, type: 'spring', stiffness: 100 }}
       >
-        <SiTailwindcss className="h-16 w-16 text-teal-500 hover:scale-110 duration-200" />
-        <span className="absolute bottom-[-40px] left-1/2 transform -translate-x-1/2 items-center justify-center hidden group-hover:flex">
-          <span className="relative z-10 p-2 px-4 text-sm leading-none text-white bg-rose-600 shadow-lg rounded-md">
+        <SiTailwindcss className="h-16 w-16 text-teal-500 hover:scale-110 duration-200 z-0" />
+        <span className="absolute bottom-[-40px] left-1/2 transform -translate-x-1/2 items-center justify-center hidden group-hover:flex z-20">
+          <span className="relative p-2 px-4 text-sm leading-none text-white bg-rose-600 shadow-lg rounded-md">
             Tailwind CSS
           </span>
         </span>
@@ -65,9 +65,9 @@ const TechStack = () => {
         animate={{ opacity: 1, y: 0 }}
         transition={{ delay: 1.0, type: 'spring', stiffness: 100 }}
       >
-        <FaJs className="h-16 w-16 text-yellow-500 hover:scale-110 duration-200" />
-        <span className="absolute bottom-[-40px] left-1/2 transform -translate-x-1/2 items-center justify-center hidden group-hover:flex">
-          <span className="relative z-10 p-2 px-4 text-sm leading-none text-white bg-rose-600 shadow-lg rounded-md">
+        <FaJs className="h-16 w-16 text-yellow-500 hover:scale-110 duration-200 z-0" />
+        <span className="absolute bottom-[-40px] left-1/2 transform -translate-x-1/2 items-center justify-center hidden group-hover:flex z-20">
+          <span className="relative p-2 px-4 text-sm leading-none text-white bg-rose-600 shadow-lg rounded-md">
             JavaScript
           </span>
         </span>
@@ -78,10 +78,74 @@ const TechStack = () => {
         animate={{ opacity: 1, y: 0 }}
         transition={{ delay: 1.2, type: 'spring', stiffness: 100 }}
       >
-        <FaNodeJs className="h-16 w-16 text-green-500 hover:scale-110 duration-200" />
-        <span className="absolute bottom-[-40px] left-1/2 transform -translate-x-1/2 items-center justify-center hidden group-hover:flex">
-          <span className="relative z-10 p-2 px-4 text-sm leading-none text-white bg-rose-600 shadow-lg rounded-md">
+        <FaNodeJs className="h-16 w-16 text-green-500 hover:scale-110 duration-200 z-0" />
+        <span className="absolute bottom-[-40px] left-1/2 transform -translate-x-1/2 items-center justify-center hidden group-hover:flex z-20">
+          <span className="relative p-2 px-4 text-sm leading-none text-white bg-rose-600 shadow-lg rounded-md">
             Node.js
+          </span>
+        </span>
+      </motion.div>
+      <motion.div
+        className="group relative flex items-center justify-center"
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 1.4, type: 'spring', stiffness: 100 }}
+      >
+        <img
+          src="/src/assets/images/react-hook-form-icon.svg"
+          className="h-16 w-16 text-green-500 hover:scale-110 duration-200 z-0"
+        />
+        <span className="absolute bottom-[-68px] left-1/2 transform -translate-x-1/2 items-center justify-center hidden group-hover:flex z-20">
+          <span className="relative p-2 px-4 text-sm leading-none text-white bg-rose-600 shadow-lg rounded-md">
+            React Hook Form
+          </span>
+        </span>
+      </motion.div>
+      <motion.div
+        className="group relative flex items-center justify-center"
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 1.6, type: 'spring', stiffness: 100 }}
+      >
+        <img
+          src="/src/assets/images/zustand-icon.svg"
+          className="h-16 w-16 text-green-500 hover:scale-110 duration-200 z-0"
+        />
+        <span className="absolute bottom-[-40px] left-1/2 transform -translate-x-1/2 items-center justify-center hidden group-hover:flex z-20">
+          <span className="relative p-2 px-4 text-sm leading-none text-white bg-rose-600 shadow-lg rounded-md">
+            Zustand
+          </span>
+        </span>
+      </motion.div>
+      <motion.div
+        className="group relative flex items-center justify-center"
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 1.8, type: 'spring', stiffness: 100 }}
+      >
+        <img
+          src="/src/assets/images/axios-icon.svg"
+          className="h-16 w-16 text-green-500 hover:scale-110 duration-200 z-0"
+        />
+        <span className="absolute bottom-[-40px] left-1/2 transform -translate-x-1/2 items-center justify-center hidden group-hover:flex z-20">
+          <span className="relative p-2 px-4 text-sm leading-none text-white bg-rose-600 shadow-lg rounded-md">
+            Axios
+          </span>
+        </span>
+      </motion.div>
+      <motion.div
+        className="group relative flex items-center justify-center"
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 2.0, type: 'spring', stiffness: 100 }}
+      >
+        <img
+          src="/src/assets/images/storybook-icon.svg"
+          className="h-16 w-16 text-green-500 hover:scale-110 duration-200 z-0"
+        />
+        <span className="absolute bottom-[-40px] left-1/2 transform -translate-x-1/2 items-center justify-center hidden group-hover:flex z-20">
+          <span className="relative p-2 px-4 text-sm leading-none text-white bg-rose-600 shadow-lg rounded-md">
+            Storybook
           </span>
         </span>
       </motion.div>
