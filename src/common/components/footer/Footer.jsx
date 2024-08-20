7157ad3fdeb2727b40208c85afdb0cffbe784e0b
Hyejinijini <qkdgpwls001@gmail.com>
2024-08-20 14:05:23 +0900

    update: Footer.jsx 파일 수정
    스타일 수정

--- src/common/components/footer/Footer.jsx
+++ src/common/components/footer/Footer.jsx
@@ -3,7 +3,7 @@ import CopyRight from '@common/components/footer/CopyRight.jsx'
 const Footer = () => {
   return (
     <footer className="text-center">
-      <div className="container mx-auto text-center pt-16 px-10 pb-10">
+      <div className="container mx-auto text-center pt-64 px-10 pb-10 md:pt-16 lg:pt-16 sm:pt-44">
         <CopyRight copyright={'Copyright © 2024 HYEHYE blog.'} />
       </div>
     </footer>
