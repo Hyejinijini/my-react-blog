ead2ca2eacb44f159f253d8c6e6d31f8152e610a
Hyejinijini <qkdgpwls001@gmail.com>
2024-08-21 17:58:53 +0900

    update: common/Header.jsx 컴포넌트 주석 추가
    코드에 대한 설명을 주석으로 추가 및 스타일 조정

--- src/common/components/header/Header.jsx
+++ src/common/components/header/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+// components
 import BlogTitle from '@common/components/header/BlogTitle.jsx'
 import Logo from '@common/components/header/Logo.jsx'
 import HeaderList from '@common/components/header/HeaderList.jsx'
@@ -8,12 +9,12 @@ const Header = () => {
   return (
     <header className="text-gray-600 bg-rose-50 border-b border-b-rose-200">
       <div className="flex p-4 pb-2 gap-3 items-center">
-        <div className="flex gap-2 w-full items-center">
+        <div className="flex gap-1 w-full items-center">
           {/* 로고 부분 */}
           <Logo className="w-12 h-12" />
 
           {/* 블로그 제목 부분 */}
-          <BlogTitle className="text-md font-bold hover:bg-rose-100 hover:rounded-md py-1.5 -my-2 text-start w-full lg:w-auto flex items-center" />
+          <BlogTitle className="text-md font-bold hover:bg-rose-100 hover:rounded-md py-1.5 -my-2 text-start w-auto px-2 flex items-center" />
         </div>
       </div>
 
