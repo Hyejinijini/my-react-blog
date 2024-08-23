1a263aff753b9a8c3321b145aee392c7393b8250
Hyejinijini <qkdgpwls001@gmail.com>
2024-08-23 18:12:56 +0900

    add: FrontImage.jsx 컴포넌트 추가
    컴포넌트 분리

---
+++ src/pages/home/components/FrontImage.jsx
@@ -0,0 +1,12 @@
+import jjangu from '@assets/images/짱구.webp'
+
+const FrontImage = () => {
+  return (
+    <>
+      {/* 이미지: 짱구 움짤 */}
+      <img src={jjangu} width={208} height={160} alt="짱구 움짤" className="w-52" />
+    </>
+  )
+}
+
+export default FrontImage
