1a64eb4654d17372df68a1a7445dd29637324573
Hyejinijini <qkdgpwls001@gmail.com>
2024-08-23 18:13:13 +0900

    add: FrontIntro.jsx 컴포넌트 추가
    컴포넌트 분리

---
+++ src/pages/home/components/FrontIntro.jsx
@@ -0,0 +1,22 @@
+const FrontIntro = () => {
+  return (
+    <>
+      {/* 인사말 제목 */}
+      <h2 className="text-xl font-semibold mb-3 pt-4">🌈🍓🎀 안녕하세요 🌸💘🎈</h2>
+
+      {/* 소개글 */}
+      <p className="mb-1 text-sm text-center">
+        저는 프론트엔드 개발자로 성장하기 위해 열심히 공부하고 있는 방혜진입니다. <br />
+        새로운 기술을 배우고, 실력을 키워나가는 과정에서 꾸준히 힘쓰고 있으며,
+        <br />
+        더욱 멋진 웹을 만들기 위해 노력하고 있습니다.
+      </p>
+
+      <p className="relative inline-block text-base text-rose-300 pt-1 mb-1 gove">
+        <span>프로필 보러가기</span>
+      </p>
+    </>
+  )
+}
+
+export default FrontIntro
