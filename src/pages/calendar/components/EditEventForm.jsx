337c70e4d5b7ff3b6f4cc3666c09353201fa2bce
Hyejinijini <qkdgpwls001@gmail.com>
2024-08-26 14:27:44 +0900

    update: EditEventForm 컴포넌트 날짜 데이터 수정 및 디테일
    1. 기존 date -> startDate 와 endDate 로 나눠서 관리
    2. 스타일 디테일 수정

--- src/pages/calendar/components/EditEventForm.jsx
+++ src/pages/calendar/components/EditEventForm.jsx
@@ -1,62 +1,125 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
 import { useForm } from 'react-hook-form'
+import { ChromePicker } from 'react-color'
 
-const EditEventForm = ({ selectedEvent, handleDeleteEvent, handleUpdateEvent }) => {
-  // useForm 훅을 사용하여 폼 상태를 관리
-  // defalutValues 옵션을 통해 폼의 초기값을 설정
-  const { register, handleSubmit } = useForm({
+const EditEventForm = ({ selectedEvent, handleDeleteEvent, handleUpdateEvent, handleCloseModal }) => {
+  const { register, handleSubmit, setValue } = useForm({
     defaultValues: {
-      title: selectedEvent.title, // 이벤트의 제목을 초기값으로 설정
-      color: selectedEvent.color // 이벤트의 색상을 초기값으로 설정
+      title: selectedEvent.title,
+      color: selectedEvent.color,
+      startDate: selectedEvent.start,
+      endDate: selectedEvent.end
     }
   })
 
+  const [showColorPicker, setShowColorPicker] = useState(false)
+  const [color, setColor] = useState(selectedEvent.color)
+  const colorPickerRef = useRef(null)
+
+  // 색상 변경 시 폼 상태와 로컬 색상 상태 업데이트
+  const handleColorChange = (color) => {
+    setColor(color.hex)
+    setValue('color', color.hex)
+  }
+
+  // 색상 선택기 클릭 시 팝업 닫힘 방지
+  const handleClick = (event) => {
+    event.stopPropagation()
+    setShowColorPicker(!showColorPicker)
+  }
+
   // 폼 제출 시 호출되는 함수
-  // data 매개변수는 사용자가 입력한 폼 데이터를 담고 있음
   const onSubmit = (data) => {
-    // handleUpdateEvent 함수에 현재 선택된 이벤트(selectedEvent)와
-    // 폼 데이터를 합친 객체를 전달하여 업데이트
     handleUpdateEvent({ ...selectedEvent, ...data })
+    handleCloseModal() // 폼 제출 후 모달 닫기
   }
 
   return (
-    <>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        {/* 이벤트 제목 입력 필드 */}
-        <div className="flex">
-          {/* react-hook-form의 register를 사용하여 필드 등록
-          required 옵션을 통해 제목 입력을 필수로 지정 */}
+    <div className="fixed inset-0 flex items-center justify-center z-50">
+      {/* 어두운 배경 */}
+      <div className="fixed inset-0 bg-black bg-opacity-50 z-40" onClick={handleCloseModal}></div>
+
+      {/* 모달 내용 */}
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="bg-white py-2 rounded-md border border-gray-300 relative z-50 w-1/2"
+      >
+        {/* 모달 제목 */}
+        <h2 className="text-lg font-bold text-gray-700 mb-4 border-b border-gray-300 pb-4 pt-2 px-4">일정 수정</h2>
+
+        {/* X 버튼
+        <button
+          type="button"
+          onClick={(event) => {
+            event.stopPropagation() // 이벤트 전파 방지
+            handleCloseModal()
+          }}
+          className="absolute top-5 right-2 text-gray-500 hover:text-gray-700 z-50"
+        >
+          <svg
+            className="w-5 h-5"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path>
+          </svg>
+        </button> */}
+
+        <label className="text-gray-700 px-4">일정</label>
+        <div className="relative mb-4 px-4 flex items-center">
           <input
             type="text"
             {...register('title', { required: true })}
-            className="border border-gray-300 focus:outline-none focus:border-rose-400 p-2 w-full rounded-md mr-4"
+            className="border border-gray-300 focus:outline-none focus:border-rose-400 p-2 w-full rounded-md pr-16"
             placeholder="이벤트 제목"
           />
 
-          {/* 색상 선택 필드 */}
-          <div className="flex justify-center items-center">
-            {/* react-hook-form의 register를 사용하여 필드 등록 */}
-            <input
-              type="color"
-              {...register('color')}
-              className="w-8 h-8 border-none cursor-pointer bg-transparent p-0 m-0"
-            />
-          </div>
+          <button
+            type="button"
+            onClick={handleClick}
+            className="absolute right-6 top-1/2 transform -translate-y-1/2 w-5 h-5 rounded-full border border-gray-300"
+            style={{ backgroundColor: color }}
+          />
+
+          {showColorPicker && (
+            <div ref={colorPickerRef} className="absolute right-0 top-full mt-2 z-10">
+              <ChromePicker color={color} onChange={handleColorChange} disableAlpha />
+            </div>
+          )}
         </div>
 
-        {/* 저장 및 삭제 버튼 그룹 */}
-        <div className="flex justify-end mt-6 gap-2">
-          {/* 저장 버튼: 폼 제출 시 onSubmit 함수 호출 */}
+        <div className="flex flex-col gap-2 mb-4 px-4">
+          <label className="text-gray-700">시작 날짜</label>
+          <input
+            type="date"
+            {...register('startDate', { required: true })}
+            className="w-full h-10 cursor-pointer bg-transparent border border-gray-300 focus:outline-none focus:border-rose-400 rounded-md mb-2 p-2"
+            style={{ border: '1px solid #d1d5db' }} // border 스타일 직접 적용
+          />
+        </div>
+
+        <div className="flex flex-col gap-2 mb-4 px-4">
+          <label className="text-gray-700">종료 날짜</label>
+          <input
+            type="date"
+            {...register('endDate')}
+            className="w-full h-10 cursor-pointer bg-transparent border border-gray-300 focus:outline-none focus:border-rose-400 p-2 rounded-md"
+            style={{ border: '1px solid #d1d5db' }} // border 스타일 직접 적용
+          />
+        </div>
+
+        <div className="flex justify-end gap-2 p-4">
           <button
-            type="submit" // 폼 제출을 트리거하는 버튼
+            type="submit"
             className="text-blue-700 duration-200 px-2 py-1 rounded text-sm hover:font-bold hover:bg-blue-700 hover:text-white"
           >
             저장
           </button>
 
-          {/* 삭제 버튼: 클릭 시 handleDeleteEvent 함수 호출 */}
           <button
-            type="button" // 버튼 클릭 시 폼이 제출되지 않도록 type을 'button'으로 설정
+            type="button"
             onClick={handleDeleteEvent}
             className="text-rose-700 px-2 py-1 rounded text-sm hover:font-bold duration-200 hover:bg-rose-700 hover:text-white"
           >
@@ -64,7 +127,7 @@ const EditEventForm = ({ selectedEvent, handleDeleteEvent, handleUpdateEvent }) 
           </button>
         </div>
       </form>
-    </>
+    </div>
   )
 }
 
