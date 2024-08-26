f2dceea873b8c790b4fa35538213a04f8840a1d5
Hyejinijini <qkdgpwls001@gmail.com>
2024-08-26 14:29:15 +0900

    update: EditEventModal 컴포넌트 제목 부분 삭제
    EditEventForm 으로 옮김

--- src/pages/calendar/components/EditEventModal.jsx
+++ src/pages/calendar/components/EditEventModal.jsx
@@ -7,10 +7,7 @@ const EditEventModal = ({ selectedEvent, handleUpdateEvent, handleDeleteEvent })
   return (
     <div className="fixed inset-0 flex items-center justify-center z-50">
       {/* 모달 컨테이너 */}
-      <div className="bg-white p-6 rounded-lg max-w-lg w-full border border-gray-300">
-        {/* 모달 제목 */}
-        <h2 className="text-xl font-bold mb-6 text-gray-700">일정 수정</h2>
-
+      <div>
         {/* 이벤트 수정 폼 */}
         <EditEventForm
           selectedEvent={selectedEvent} // 현재 수정 중인 이벤트 데이터를 전달
