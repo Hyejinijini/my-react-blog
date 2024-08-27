c51cba9419494fb89d047ad72de084936bd1dae7
Hyejinijini <qkdgpwls001@gmail.com>
2024-08-27 17:18:03 +0900

    update: 스토리북 기본 버튼 코드 수정
    기본으로 제공되는 예제 버튼 코드를 내가 만든 공통 컴포넌트 버튼 스타일과 동일하게 변경함

--- src/stories/Button.jsx
+++ src/stories/Button.jsx
@@ -1,25 +1,44 @@
-import React from 'react';
-import './button.css';
+import React from 'react'
 
-/**
- * Primary UI component for user interaction
- */
-export const Button = ({
-  primary = false,
-  size = 'medium',
+const Button = ({
+  className,
+  primary,
   backgroundColor,
+  size,
   label,
+  padding,
+  margin,
+  width,
+  height,
+  onClick,
   ...props
 }) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+  const baseStyle = 'py-1 bg-rose-400 font-bold border rounded-md text-white duration-100 focus:outline-none focus:ring'
+
+  const sizeStyle = {
+    small: 'text-sm py-2 px-4',
+    medium: 'text-md py-3 px-6',
+    large: 'text-lg py-4 px-8'
+  }
+
+  const customStyle = {
+    padding: padding ? padding : '',
+    margin: margin ? margin : '',
+    width: width ? width : '',
+    height: height ? height : ''
+  }
+
   return (
     <button
       type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
-      style={{ backgroundColor }}
+      className={`${baseStyle} ${sizeStyle[size]} ${className}`}
+      style={customStyle}
+      onClick={onClick}
       {...props}
     >
       {label}
     </button>
-  );
-};
+  )
+}
+
+export default Button
