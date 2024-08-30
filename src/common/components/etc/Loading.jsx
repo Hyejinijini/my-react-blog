48c6e344620c50c54f6b1c81685cd31be6d33420
Hyejinijini <qkdgpwls001@gmail.com>
2024-08-30 17:49:14 +0900

    update: Loading 컴포넌트 위치 조정
    로딩바가 나타나는 위치 조정

--- src/common/components/etc/Loading.jsx
+++ src/common/components/etc/Loading.jsx
@@ -2,10 +2,9 @@ import React from 'react'
 
 const Loading = ({ className }) => {
   return (
-    <div
-      className={`flex justify-center items-center h-screen">
-      <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-blue-500 ${className}`}
-    ></div>
+    <div className={`flex justify-center items-center h-screen ${className}`}>
+      <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-blue-500"></div>
+    </div>
   )
 }
 
