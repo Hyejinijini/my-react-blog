23519156acd7a7be26b1ae0991b93a4130960c7f
Hyejinijini <qkdgpwls001@gmail.com>
2024-08-30 16:15:18 +0900

    update: HomeMain 스타일 조정
    Test 컴포넌트와 footer 컴포넌트가 겹쳐보이는 문제를 스타일 조정을 통해 해결 -> margin-bottom 조정

--- src/pages/home/components/HomeMain.jsx
+++ src/pages/home/components/HomeMain.jsx
@@ -2,7 +2,7 @@ import MyInfo from '@pages/home/components/MyInfo.jsx'
 
 const HomeMain = () => {
   return (
-    <main className="flex-1 sm:mx-0 sm:mt-6 sm:p-4 md:p-1 md:mr-4 md:ml-0 mx-0 p-4 mt-6 lg:p-2">
+    <main className="flex-1 sm:mx-0 sm:mt-6 sm:p-4 md:p-1 md:mr-4 md:ml-0 mx-0 p-4 mt-6 lg:p-2 md:mb-96 sm:mb-96 mb-40">
       {/* 소개 부분 */}
       <MyInfo />
     </main>
