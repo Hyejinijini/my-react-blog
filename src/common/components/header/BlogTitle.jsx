06a4525381a67557b1ada88374124392c3c7eb57
Hyejinijini <qkdgpwls001@gmail.com>
2024-08-30 15:21:50 +0900

    update: BlogTitle 컴포넌트 title prop 추가
    storybook 에서 더 다양한 테스트를 해볼 수 있게 하기 위해서 title prop 을 추가함

--- src/common/components/header/BlogTitle.jsx
+++ src/common/components/header/BlogTitle.jsx
@@ -1,9 +1,9 @@
-const BlogTitle = ({ className }) => {
+const BlogTitle = ({ className, title = 'HYEHYE' }) => {
   return (
     <>
       {/* 블로그 제목 */}
       <a href="/" className={className}>
-        HYEHYE
+        {title}
       </a>
     </>
   )
