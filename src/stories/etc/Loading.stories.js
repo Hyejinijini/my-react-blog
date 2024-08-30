7432f46dcb2ee07c70c1fcc1453a3caaa5fba95b
Hyejinijini <qkdgpwls001@gmail.com>
2024-08-30 15:22:38 +0900

    add: Loading 공통 컴포넌트 storybook 에 등록
    공통 컴포넌트인 Loading 컴포넌트를 스토리북에 등록하여 다양한 테스트를 할 수 있도록 함

---
+++ src/stories/etc/Loading.stories.js
@@ -0,0 +1,16 @@
+import Loading from '@common/components/etc/Loading.jsx'
+
+export default {
+  title: 'Components/Loading',
+  component: Loading,
+  parameters: {
+    layout: 'centered'
+  },
+  tags: ['autodocs']
+}
+
+export const Default = {
+  args: {
+    className: ''
+  }
+}
