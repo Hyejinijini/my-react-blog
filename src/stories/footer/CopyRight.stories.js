91d9a8f74903d465469446a328e479ce518a9c7a
Hyejinijini <qkdgpwls001@gmail.com>
2024-08-30 15:22:59 +0900

    add: CopyRight 컴포넌트 스토리북 등록
    공통 컴포넌트인 CopyRight 컴포넌트를 스토리북에 등록하여 다양한 테스트를 할 수 있도록 함

---
+++ src/stories/footer/CopyRight.stories.js
@@ -0,0 +1,17 @@
+import CopyRight from '@common/components/footer/CopyRight.jsx'
+
+export default {
+  title: 'Components/CopyRight',
+  component: CopyRight,
+  parameters: {
+    layout: 'centered'
+  },
+  tags: ['autodocs']
+}
+
+export const Default = {
+  args: {
+    copyright: 'Copyright © 2024 HYEHYE blog.',
+    className: ''
+  }
+}
