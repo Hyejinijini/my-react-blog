e9d327b80af90af888fa2a85c34706ad82c5762a
Hyejinijini <qkdgpwls001@gmail.com>
2024-09-02 14:41:18 +0900

    update: apiClient 파일 주석 추가
    코드 설명에 대한 주석 추가

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -1,10 +1,17 @@
 import apiClient from '@api/config.js'
 
+// 이 함수는 지정된 url 로 get 요청을 보내고, 서버로부터 받은 데이터를 반환한다.
+// url: 요청을 보낼 API 의 URL
+// params: 요청에 사용할 쿼리 파라미터
 export const getRequest = async (url, params) => {
   try {
+    // Axios 를 통해 주어진 URL 로 GET 요청을 보냄
     const response = await apiClient.get(url, { params })
+    // 서버로부터의 응답에서 실제 데이터를 반환
     return response.data
   } catch (error) {
+    // 에러 처리
+    // 요청이 실패하면, 콘솔에 에러 메시지를 출력하고, 에러를 호출한 함수로 다시 던짐
     console.error('GET request failed:', error)
     throw error
   }
