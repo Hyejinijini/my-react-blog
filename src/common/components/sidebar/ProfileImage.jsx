5cf22b08f5232d0e6e9cedffe2358fd2fa21a1f6
방혜진 <hyejin@banghyejin-ui-MacBookAir.local>
2024-09-24 18:24:38 +0900

    update: ProfileImage 컴포넌트 다크모드 스타일 추가
    - 다크모드 일 때, 보여질 스타일 추가

--- src/common/components/sidebar/ProfileImage.jsx
+++ src/common/components/sidebar/ProfileImage.jsx
@@ -7,7 +7,7 @@ const ProfileImage = ({ profile }) => {
           <img
             src={profile.profileImage}
             alt="프로필 이미지"
-            className="rounded-full w-24 h-24 ml-0 mr-4 mt-8 border-2 border-rose-200 sm:w-28 sm:h-28 md:w-64 md:h-64 lg:w-72 lg:h-72"
+            className="rounded-full w-24 h-24 ml-0 mr-4 mt-8 border-2 border-rose-200 dark:border-customGrayMid sm:w-28 sm:h-28 md:w-64 md:h-64 lg:w-72 lg:h-72"
           />
         </div>
       </div>
