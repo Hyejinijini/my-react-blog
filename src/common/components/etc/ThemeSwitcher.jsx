1181cd6921a714d0ea79d6107378eaf03284efb5
방혜진 <hyejin@banghyejin-ui-MacBookAir.local>
2024-09-30 15:49:25 +0900

    update: ThemeSwitcher 컴포넌트 주석 추가
    - 주석 추가

--- src/common/components/etc/ThemeSwitcher.jsx
+++ src/common/components/etc/ThemeSwitcher.jsx
@@ -7,14 +7,18 @@ import { MdDarkMode, MdOutlineLightMode } from 'react-icons/md'
 
 const ThemeSwitcher = () => {
   const { isDarkMode, toggleDarkMode } = useThemeStore()
+
+  // 헌재 경로를 가져오기 위한 useLocation 훅 사용
   const location = useLocation()
 
+  // Not Found 페이지가 아닌지 확인하는 조건
   const isNotFoundPage =
     location.pathname !== '/' && location.pathname !== '/guestbook' && location.pathname !== '/calendar'
 
   return (
     // 다크 모드 상태에 따라 클래스 이름 변경
     <div className="absolute transform z-10 top-4 right-4">
+      {/* NotFound 페이지가 아닐 경우에만 버튼 표시 */}
       {!isNotFoundPage && (
         <button onClick={toggleDarkMode}>
           {/* 현재 모드에 따라 버튼 텍스트 변경 */}
